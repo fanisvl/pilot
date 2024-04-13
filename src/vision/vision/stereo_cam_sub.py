add78506e08063435223a8a12c4cd61a9f001b15
fanisvl <fanis.vlahogiannis@gmail.com>
2024-04-13 11:08:10 +0300

    Working inference on subscriber

--- src/vision/vision/stereo_cam_sub.py
+++ src/vision/vision/stereo_cam_sub.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
+from ultralytics import YOLO
 
 class StereoCamSub(Node):
 
@@ -15,7 +16,8 @@ class StereoCamSub(Node):
             self.image_callback,
             10
         )
-
+        
+        self.model = YOLO('model/yolov8s700.pt') 
         self.bridge = CvBridge()
         self.get_logger().info("stereo_cam Subscriber initialized.")
 
@@ -27,10 +29,25 @@ class StereoCamSub(Node):
             self.get_logger().error("Error converting ROS Image to OpenCV image: {0}".format(e))
             return
 
+        annotated_image = self.inference(cv_image)
+
         # Display the image
-        cv2.imshow("Camera Image", cv_image)
+        cv2.imshow("Camera Image", annotated_image)
         cv2.waitKey(1)
 
+    def inference(self, image):  
+        result = self.model.predict(image)[0]
+        boxes = result.boxes  # Boxes object for bbox outputs
+        for box in boxes:
+            coordinates = box.xyxy[0]
+            x1, y1, x2, y2 = int(coordinates[0]), int(coordinates[1]), int(coordinates[2]), int(coordinates[3])
+            conf = box.conf.item()
+            class_pred = box.cls.item()
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            label = f"{self.model.names[class_pred]}: conf: {conf}"
+            cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+        
+        return image
 
 def main(args=None):
     rclpy.init(args=args)
