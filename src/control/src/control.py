3aa42efa7b1cc82a91ad2f2dcca41755df036486
fanisvl <fanis.vlahogiannis@gmail.com>
2024-09-20 13:47:53 +0300

    Add v0 control

--- src/control/src/control.py
+++ src/control/src/control.py
@@ -1,85 +1,60 @@
 #!/usr/bin/python3
 
 import rospy
-from messages.msg import CenterLinePoints, Point
+from messages.msg import Points, Point
 import math
 import numpy as np
 
 class Control:
     def __init__(self):
         rospy.init_node("control_node")
-        self.centerline_sub =  rospy.Subscriber("centerline_points", CenterLinePoints, self.control)
+        self.tajectory_sub =  rospy.Subscriber("trajectory", Points, self.control)
 
         self.WHEELBASE_LEN = 0.5
         self.PURE_PURSUIT_L = 3
 
-        self.TURN_MIN = 0.0
-        self.TURN_A   = 0.25
-        self.TURN_MID = 0.50
-        self.TURN_B   = 0.75
-        self.TURN_MAX = 1.0
+        self.TURN_THRESHOLDS = [
+            (1.0,   0.00),    # TURN_MIN
+            (1.14,  0.25),    # TURN_A
+            (1.28,  0.50),    # TURN_MID
+            (1.42,  0.75),    # TURN_B
+            (1.57,  1.00)     # TURN_MAX
+        ]
 
 
-    def pure_pursuit(self, centerline_points):
-        L = self.PURE_PURSUIT_L
-        while True:
-            outerpoints = []
-            innerpoints = []
-            innerpoints.append((0,0))
-            for point in centerline_points:
-                point = (point.x, point.y)
-                if self.euclidean_distance_origin(point) >= L:
-                    outerpoints.append(point)
-                else:
-                    innerpoints.append(point)
+    def pure_pursuit(self, trajectory_points, L=5):
+        while L > 0:
+            outerpoints = [point for point in trajectory_points if self.euclidean_distance_origin((point.x, point.y)) >= L]
+            innerpoints = [point for point in trajectory_points if self.euclidean_distance_origin((point.x, point.y)) < L]
+            
+            if outerpoints:
+                min_outer = min(outerpoints, key=lambda p: self.euclidean_distance_origin((p.x, p.y)))
+                max_inner = max(innerpoints, key=lambda p: self.euclidean_distance_origin((p.x, p.y))) if innerpoints else Point(0, 0, 0)
+                return self.interpolate_to_circle((0, 0), L, (max_inner.x, max_inner.y), (min_outer.x, min_outer.y))
+            
+            L *= 0.9
+        
+        raise ValueError("Pure Pursuit: No suitable points found")
 
-            if len(outerpoints) != 0:
-                break
-
-            elif L == 0:
-                raise ValueError("Pure Pursuit: L = 0")
-
-            else:
-                L *= 0.9
-
-        min_outer = min(outerpoints, key=self.euclidean_distance_origin)
-        max_inner = max(innerpoints, key=self.euclidean_distance_origin)
-        target_point = self.interpolate_to_circle((0,0), L, max_inner, min_outer)
-        tx, ty = target_point[0], target_point[1]
-
-        arc_curvature = math.degrees((2*tx) / math.pow(L, 2.0))
+    def steering_angle(self, target):
+        """
+        INPUT: Target point
+        Steering angle output from pure pursuit ranges from (-1.57, 1.57) = (-pi/2, pi/2)
+        OUTPUT: Steering angle normalized to (-1, 1)
+        """
+        tx, ty = target
+        arc_curvature = math.degrees((2*tx) / (self.PURE_PURSUIT_L**2))
         steering_angle = math.atan(arc_curvature * self.WHEELBASE_LEN)
-
-        return steering_angle
-
-    def normalize_steering_angle(self, steering_angle):
-        """
-        Helper function for control
-        INPUT: Steering angle output from pure pursuit ranges from (-1.57, 1.57) = (-pi/2, pi/2)
-        OUTPUT: Normalize to (-1, 1)
-        """
-        # determine turn heading
-        if steering_angle >= 0:
-            turn_heading = 1 # right
-        else:
-            turn_heading = -1 # left
-
-        normalized_steering_angle = turn_heading
-        steering_angle = abs(steering_angle)
-
-        # Normalize the steering angle
-        if steering_angle < 1:
-            normalized_steering_angle *= self.TURN_MIN
-        elif 1 <= steering_angle < 1.1425:
-            normalized_steering_angle *= self.TURN_A
-        elif 1.1425 <= steering_angle < 1.285:
-            normalized_steering_angle *= self.TURN_MID
-        elif 1.285 <= steering_angle < 1.4275:
-            normalized_steering_angle *= self.TURN_B
-        elif 1.4275 <= steering_angle <= 1.57:
-            normalized_steering_angle *= self.TURN_MAX
-
-        return normalized_steering_angle
+        
+        turn_heading = 1 if steering_angle >= 0 else -1
+        abs_steering_angle = abs(steering_angle)
+        
+        for threshold, value in self.TURN_THRESHOLDS:
+            if abs_steering_angle <= threshold:
+                return turn_heading * value
+        
+        rospy.logwarn(f"Couldn't find valid turn threshold for steering angle: {abs_steering_angle}")
+        return 0
 
     def euclidean_distance_origin(self, point):
         return math.sqrt(point[0]**2 + point[1]**2)
@@ -88,38 +63,35 @@ class Control:
         x_c, y_c = center
         x_inner, y_inner = inner_point
         x_outer, y_outer = outer_point
-
         vx = x_outer - x_inner
         vy = y_outer - y_inner
 
         A = vx**2 + vy**2
         B = 2 * (vx * (x_inner - x_c) + vy * (y_inner - y_c))
         C = (x_inner - x_c)**2 + (y_inner - y_c)**2 - radius**2
-
+        
         discriminant = B**2 - 4 * A * C
-
         if discriminant < 0:
             raise ValueError("The points do not form a line that intersects the circle.")
         t1 = (-B + np.sqrt(discriminant)) / (2 * A)
         t2 = (-B - np.sqrt(discriminant)) / (2 * A)
-
         t = max(t1, t2)
-
         x_interp = x_inner + t * vx
         y_interp = y_inner + t * vy
 
         return (x_interp, y_interp)
 
-    def control(self, centerline_points_msg):
+    def control(self, trajectory_msg):
         """
         For speed just use a very low value constant
-        INPUT: Steering angle from pure pursuit
+        INPUT: Trajectory points from planning
         OUTPUT: Steering commands to actuators
         """
-        centerline_points = centerline_points_msg.centerline
-        steering_angle = self.pure_pursuit(centerline_points)
-        normalized = self.normalize_steering_angle(steering_angle)
-        print(f"Normalized: {normalized}")
+        trajectory_points = trajectory_msg.points
+
+        target = self.pure_pursuit(trajectory_points, self.PURE_PURSUIT_L)
+        steering_angle = self.steering_angle(target)
+
 
 if __name__ == "__main__":
     node = Control()
