3ffbcf00d8d1af7bed904737dee992644238b575
fanisvl <fanis.vlahogiannis@gmail.com>
2024-09-24 17:36:27 +0300

    Fix steering angle sub

--- src/operation/src/map.py
+++ src/operation/src/map.py
@@ -19,13 +19,13 @@ class LiveMap:
         self.cones = []
         self.trajectory = []
         self.target = []
-        self.steering = 0
+        self.steering = None
         self.steering_angle = 0
 
         self.sub_cones = rospy.Subscriber("/cone_estimates", ConeEstimates, self.cones_callback, queue_size=1)
         self.sub_trajectory = rospy.Subscriber("/trajectory", Points, self.trajectory_callback, queue_size=1)
-        self.sub_steering = rospy.Subscriber("/control/steering", Float32, self.steering_angle_callback, queue_size=1)
-        self.sub_steering = rospy.Subscriber("/control/steering_angle", Float32, self.steering_callback, queue_size=1)
+        self.sub_steering = rospy.Subscriber("/control/steering", Float32, self.steering_callback, queue_size=1)
+        self.sub_steering_angle = rospy.Subscriber("/control/steering_angle", Float32, self.steering_angle_callback, queue_size=1)
         self.sub_target = rospy.Subscriber("/control/target", Point, self.target_callback, queue_size=1)
 
         
@@ -40,11 +40,11 @@ class LiveMap:
     
     def steering_callback(self, msg):
         self.steering = msg.data
-        print(f"Steering Angle: {msg.data}")
+        print(f"Steering: {msg.data}")
 
     def steering_angle_callback(self, msg):
         self.steering_angle = msg.data
-        print(f"Steering: {msg.data}")
+        print(f"Steering Angle: {msg.data}")
 
     def target_callback(self, msg):
         with self.lock:
@@ -54,13 +54,12 @@ class LiveMap:
     def plt_func(self, _):
         with self.lock:
             self.ax.clear()  # Clear previous points
-            # Plot cone points
             if self.cones:
                 x_cones, y_cones = zip(*self.cones)
                 self.ax.scatter(x_cones, y_cones, color="orange", label="Cones")  # Plot cone points
                 for x, y in zip(x_cones, y_cones):
                     self.ax.text(x, y + 0.3, f'({x:.0f}, {y:.0f})', fontsize=8, ha='center')
-            # Plot trajectory points
+
             if self.trajectory:
                 x_trajectory, y_trajectory = zip(*self.trajectory)
                 self.ax.scatter(x_trajectory, y_trajectory, color="blue", label="Trajectory", s=20)
@@ -68,22 +67,22 @@ class LiveMap:
             if self.target:
                 self.ax.scatter(self.target[0], self.target[1], color='red', label="Target")
 
-            if self.steering:
+            if self.steering != None:
                 self.ax.text(
                     0.05, 0.95,                      
                     f"Steering: {self.steering}",
-                    transform=self.ax.transAxes,     # Use axes coordinates for positioning
-                    fontsize=12,                     # Font size of the text
-                    verticalalignment='top',         # Align the text to the top
-                    horizontalalignment='left',      # Align the text to the left
-                    bbox=dict(facecolor='white', alpha=0.5)  # Optional: Add a background box with transparency
+                    transform=self.ax.transAxes,
+                    fontsize=12,
+                    verticalalignment='top',  
+                    horizontalalignment='left',
+                    bbox=dict(facecolor='white', alpha=0.5) 
                 )
             
             self.ax.set_xlim(-100, 100)
             self.ax.set_ylim(0, 500)
             self.ax.set_title("Map")
-            self.ax.set_xlabel("X")
-            self.ax.set_ylabel("Z")
+            self.ax.set_xlabel("X (cm)")
+            self.ax.set_ylabel("Z (cm)")
             plt.grid()
             self.ax.legend()  # Show legend
             return self.ax
@@ -91,6 +90,7 @@ class LiveMap:
     def plt_show(self):
         """Start the matplotlib animation."""
         self.ani = anim.FuncAnimation(self.fig, self.plt_func, interval=1000)
+        plt.savefig('plot.png', dpi=300, bbox_inches='tight')
         plt.show()
 
 def main():
